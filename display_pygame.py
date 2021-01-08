9ce84b63a97b94da77761d324fa215f5faa78da0
Lukasz <skotniczny@outlook.com>
2021-01-08 23:36:30 +0100

    Handle mouse and keyboard events, improve board display

--- display_pygame.py
+++ display_pygame.py
@@ -1,4 +1,5 @@
 from engine import GameOfLife
+import math
 import pygame
 
 
@@ -10,26 +11,20 @@ def run(board: GameOfLife, box_size):
     # Create empty pygame surface.
     background = pygame.Surface(screen.get_size())
     # Fill the background white color.
-    background.fill((255, 255, 255))
-    pygame.draw.rect(background, "#ff0000", [50, 50, 50, 50])
-    # # Convert Surface object to make blitting faster.
-    # background = background.convert()
-    # # Copy background to screen (position (0, 0) is upper left corner).
-    # screen.blit(background, (0, 0))
+    background.fill("#ffffff")
     # Create Pygame clock object.
     clock = pygame.time.Clock()
 
-    pygame.draw.rect(background, "#ff0000", [50, 50, 50, 50])
-
     mainloop = True
+    is_stopped = False
     # Desired framerate in frames per second. Try out other values.
-    FPS = 30
+    fps = 30
     # How many seconds the "game" is played.
     playtime = 0.0
     board.generate()
     while mainloop:
         # Do not go faster than this framerate.
-        milliseconds = clock.tick(FPS)
+        milliseconds = clock.tick(fps)
         playtime += milliseconds / 1000.0
 
         for event in pygame.event.get():
@@ -38,22 +33,33 @@ def run(board: GameOfLife, box_size):
                 mainloop = False
             elif event.type == pygame.KEYDOWN:
                 # User presses ESCAPE-Key
+                if event.key == pygame.K_SPACE:
+                    is_stopped = not is_stopped
                 if event.key == pygame.K_ESCAPE:
                     mainloop = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed()[0]:
+                    (x, y) = pygame.mouse.get_pos()
+                    cell_x = math.floor(x / box_size)
+                    cell_y = math.floor(y / box_size)
+                    cell_index = cell_x + cell_y * board.width
+                    board.toggle_cell(cell_index)
 
         # Print framerate and playtime in titlebar.
         text = "FPS: {0:.2f}   Playtime: {1:.2f}".format(clock.get_fps(), playtime)
         pygame.display.set_caption(text)
-        board.next_generation()
-        background.fill("#ffffff")
-        for i in range(board.size):
-            if board.grid[i]:
-                col = i % board.width
-                row = int((i - col) / board.width)
-                color = "#3f51b5" if board.grid[i] else "#ffffff"
-                left = col * box_size
-                top = row * box_size
-                background.fill(color, [left, top, box_size, box_size])
+        if not is_stopped:
+            board.next_generation()
+            background.fill("#ffffff")
+            for i in range(board.size):
+                if board.grid[i]:
+                    col = i % board.width
+                    row = int((i - col) / board.width)
+                    color = "#3f51b5"
+                    left = col * box_size + 1
+                    top = row * box_size + 1
+                    size = box_size - 1
+                    background.fill(color, [left, top, size, size])
         # Update Pygame display.
         # Convert Surface object to make blitting faster.
         background = background.convert()
@@ -66,4 +72,3 @@ def run(board: GameOfLife, box_size):
 
     # At the very last:
     print("This game was played for {0:.2f} seconds".format(playtime))
-
